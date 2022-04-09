236e4b9746d6c37634e3738d289fbd9ecd65b296
Arnav Sharma <arnav1776@gmail.com>
2022-04-09 18:16:46 +0530

    Database connection

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 const express = require('express')
+const db = require("./database");
 const app = express()
 const port = 3000
 
 // setting ejs as default templating engine
 app.set('view engine','ejs');
 
+// initiate database connection
+db.init();
+
+
 app.get('/', (req, res) => {
-	res.send('Hello World!')
+	res.render('home');
+})
+
+
+app.get('/login', (req, res) => {
+	res.render('login');
+})
+
+app.get('/signup', (req, res) => {
+	res.render('signup');
 })
 
 app.listen(port, () => {
