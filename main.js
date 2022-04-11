46accd4b42bd90a60d43159ac5dc4f41c08c6f56
Arnav Sharma <arnav1776@gmail.com>
2022-04-11 22:41:39 +0530

    views

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const multer  = require('multer');
 const db = require("./database");
+const fs  = require('fs');
+var session = require('express-session')
 const app = express()
 const port = 3000
+
+const userModel = require("./database/models/user.js");
 
 // setting ejs as default templating engine
 app.set('view engine','ejs');
@@ -11,6 +15,8 @@ app.set('view engine','ejs');
 db.init();
 
 // middlewares
+app.use(express.static("public"));
+app.use(express.static("uploads"));
 app.use(express.urlencoded())
 
 app.get('/', function(req, res) {
@@ -30,25 +36,75 @@ const storage = multer.diskStorage({
   
   const upload = multer({ storage: storage })
   
+app.get('/', function(req, res)
+  {
+	  res.render('login');
+  })
 
-app.get('/login', function(req, res) {
+app.get('/login', function(req, res)
+{
 	res.render('login');
 })
 
-app.route("/signup").get(function(req, res) 
+app.post("/login", function(req, res)
 {
-	res.render('signup');
+	const username = req.body.username;
+	const password = req.body.password;
 
-}).post(upload.single("profile_pic"), (function(req, res){
-const username = req.body;
-const password = req.body;
-const file = req.file;
+	userModel.findOne({ username: username, password: password })
+	.then(function(user)
+	{
+		req.session.isLoggedIn = true;
+		req.session.user = user;
 
-console.log(username, password, file);
+		res.redirect("/home");
+		
+	})
 })
 
-)
+app.route("/signup").get(function(req, res)
+{
+	res.render('signup',{ error: "" });
 
+}).post(upload.single("profile_pic"), function(req, res)
+{
+	const username = req.body.username;
+	const password = req.body.password;
+	const file = req.file;
+
+	if(!username)
+	{
+		res.render("signup",{ error: "Please enter username" });
+		return
+	}
+
+	if(!password)
+	{
+		res.render("signup",{ error: "Please enter password" });
+		return
+	}
+
+	if(!file)
+	{
+		res.render("signup",{ error: "sayad profile pic bhul gye app" });
+		return
+	}
+
+	userModel.create({ 
+		username: username ,
+		password: password,
+		profile_pic: file.filename
+	})
+	.then(function()
+	{
+		res.redirect("/login");
+	})
+	.catch(function(err)
+	{	
+		//console.log(err)
+		res.render("signup",{ error: "something went terribly wrong" });
+	})
+})
 
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`)
