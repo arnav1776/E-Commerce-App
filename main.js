4c8254baa35102930f0ebe3cbe8db5c3111ff9dd
Arnav Sharma <arnav1776@gmail.com>
2022-04-12 19:21:26 +0530

    story-1

--- main.js
+++ main.js
@@ -1,45 +1,56 @@
 const express = require('express');
 const multer  = require('multer');
-const db = require("./database");
 const fs  = require('fs');
-var session = require('express-session')
+const db = require("./database");
+var session = require('express-session');
+
 const app = express()
 const port = 3000
+var count = 0;
 
 const userModel = require("./database/models/user.js");
 
+
 // setting ejs as default templating engine
-app.set('view engine','ejs');
+
+app.set('view engine', 'ejs');
 
 // initiate database connection
+
 db.init();
 
-// middlewares
-app.use(express.static("public"));
-app.use(express.static("uploads"));
+//middlewares
+
+app.use(express.static("asset"));
+app.use(express.static("uploads"))
 app.use(express.urlencoded())
 
-app.get('/', function(req, res) {
-	res.render('home');
+app.use(session({
+  secret: 'bahut badhiya wala secert spice',
+  resave: false,
+  saveUninitialized: true,
+
+}))
+
+
+// multer
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix)
+  }
 })
 
-//multer
-const storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-	  cb(null, 'uploads')
-	},
-	filename: function (req, file, cb) {
-	  const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-	  cb(null, file.fieldname + '-' + uniqueSuffix)
-	}
-  })
-  
-  const upload = multer({ storage: storage })
-  
+const upload = multer({ storage: storage })
+
 app.get('/', function(req, res)
-  {
-	  res.render('login');
-  })
+{
+	res.render('login');
+})
 
 app.get('/login', function(req, res)
 {
@@ -57,8 +68,7 @@ app.post("/login", function(req, res)
 		req.session.isLoggedIn = true;
 		req.session.user = user;
 
-		res.redirect("/home");
-		
+		res.redirect("/home");		
 	})
 })
 
@@ -86,7 +96,7 @@ app.route("/signup").get(function(req, res)
 
 	if(!file)
 	{
-		res.render("signup",{ error: "sayad profile pic bhul gye app" });
+		res.render("signup",{ error: "Please enter profile pic" });
 		return
 	}
 
@@ -106,6 +116,32 @@ app.route("/signup").get(function(req, res)
 	})
 })
 
+app.get("/home", function(req, res)
+{
+	// only logged in user should come here
+
+	const user = req.session.user;
+
+
+	fs.readFile("products.js","utf-8", function(err, data)
+	{
+		res.render("index", 
+		{ 
+			name: user.username,
+			pic: user.profile_pic,
+			products: JSON.parse(data),
+			proNum : count
+		})
+	})
+})
+
+
+app.get("/getproduct", function(req,res){
+
+   count = count + 5;
+
+})
+
 app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`)
+	console.log(`App is live at http://localhost:${port}`)
 })
---
+++ products.js
@@ -0,0 +1,71 @@
+[
+   { 
+    "name":"Watches",
+    "price":"2000-10000",
+    "description":"Buy men's and women's watches from a wide range of stylish collections. Buy now! Best Deals.",
+    "productPic": "https://michaelkors.scene7.com/is/image/MichaelKors/MK8941-0001_IS?$ProductMain$&fmt=pjpeg"
+    },
+
+ { 
+   "name":"Laptops",
+   "price":"30000-70000",
+   "description":"Get the best Laptops Online",
+   "productPic": "https://m.media-amazon.com/images/I/61s7sJEpsVL._SX425_.jpg"
+  },
+
+ { 
+   "name":"Mobile Phones",
+   "price":"5000-40000",
+   "description":"Shop for Android, iOS, Dual-SIM mobiles & More.",
+    "productPic": "https://www.thechennaimobiles.com/image/cache/catalog/ip13pnk-600x600.jpg" 
+  },
+
+ { 
+   "name":"Clothing",
+   "price":"500-1000",
+   "description":"Explore the latest collection of polo t-shirts, crew neck t-shirts, sleeveless t-shirts",
+    "productPic": "https://cf.shopee.in/file/68ea5a1636338410d7067ed093d632cc" 
+  },
+
+ {
+    "name":"Grocery",
+    "price":"10-300",
+    "description":"Find grocery items of best quality available at the lowest price.",
+    "productPic": "https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Z3JvY2VyeXxlbnwwfHwwfHw%3D&w=1000&q=80" 
+  },
+
+ { 
+   "name":"Sports Equipment",
+   "price":"1000-15000",
+   "description":"Explore our collection of sporting gear,gadgets and much more for all sports including Cricket, Badminton, Lawn Tennis, Golf, Basketball, Table Tennis etc",
+   "productPic": "https://previews.123rf.com/images/lightwise/lightwise1301/lightwise130100068/17472622-sports-equipment.jpg"
+  },
+
+ {
+    "name":"Pet Suplies",
+   "price":"200-2000",
+    "description":"Shop from largest pet supplies store",
+    "productPic": "https://www.gannett-cdn.com/presto/2020/04/10/USAT/eb927ba8-c0c6-4f11-bd21-43e5781e2540-petsmart.png?width=660&height=372&fit=crop&format=pjpg&auto=webp"
+ },
+ { 
+   "name":"Utensils",
+  "price":"500-5000",
+ "description":"Select from the best range of cooking utensils, Tongs, Ladles, Spoons, Spatulas, Whisks, Skimmers etc",
+ "productPic": "https://m.media-amazon.com/images/I/61bB+hQDQ6L._SY450_.jpg" 
+},
+
+ { 
+   "name":"Accesories",
+  "price":"100-3000",
+ "description":" Refresh your look with hats, scarves, belts and more",
+ "productPic": "https://media.istockphoto.com/photos/top-view-of-female-fashion-accessories-picture-id531786318?k=20&m=531786318&s=612x612&w=0&h=V7bp-KSd-SCn-Ud8JmgSbDc4EL3XHgq91eUEVJDEad4="
+},
+
+ {
+  "name":"ACs",
+  "price":"25000-60000",
+   "description":"Air Conditioners",
+   "productPic": "https://cdn.pixabay.com/photo/2021/09/08/07/20/air-conditioner-6605973__340.jpg" 
+  }
+
+  ]
